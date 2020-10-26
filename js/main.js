988edc763826a9db028672d577dbe5f477c600e6
Dinkie <david.churchill72@gmail.com>
2020-10-26 13:24:24 +0000

    Media Query and small linkedin button

---
+++ js/main.js
@@ -0,0 +1,30 @@
+var loaded = false;
+var currentSize = "large";
+
+$(document).ready(function(){
+	console.log("init");
+	loaded = true
+	checkWindowSize();
+});
+
+$(window).resize(checkWindowSize);
+
+function checkWindowSize(){
+	if(window.innerWidth < 900){
+		swapLogo("small");
+	}
+	else{
+		swapLogo("large");
+	}
+}
+
+function swapLogo(size){
+	if(size == "small" && currentSize == "large"){
+		$('#linkedInButton img')[0].src = "./assets/LI-In-Bug.png";
+		currentSize = "small";
+	}
+	else if (size == "large" && currentSize == "small"){
+		$('#linkedInButton img')[0].src = "./assets/LI-Logo.png";
+		currentSize = "large";
+	}
+}
