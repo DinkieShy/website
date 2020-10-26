e0d7192972d4cc262863d0c0ca1d17491d105d89
Dinkie <david.churchill72@gmail.com>
2020-10-26 13:47:14 +0000

    Meta tags and little github logo. As a treat.

--- js/main.js
+++ js/main.js
@@ -21,10 +21,12 @@ function checkWindowSize(){
 function swapLogo(size){
 	if(size == "small" && currentSize == "large"){
 		$('#linkedInButton img')[0].src = "./assets/LI-In-Bug.png";
+		$('#githubButton img')[0].src = "./assets/GitHub-Mark-Light-64px.png";
 		currentSize = "small";
 	}
 	else if (size == "large" && currentSize == "small"){
 		$('#linkedInButton img')[0].src = "./assets/LI-Logo.png";
+		$('#githubButton img')[0].src = "./assets/GitHub_Logo_White.png";
 		currentSize = "large";
 	}
 }
